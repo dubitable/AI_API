2126231c022b161a588b33cdee3fd107cf100773
dubitable <pierrequereuil@gmail.com>
2021-07-23 14:11:31 -0700

    Image POST requests working

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, url_for, redirect, jsonify
+from werkzeug.utils import secure_filename
+from PIL import Image
+
+UPLOAD_FOLDER = "/uploads"
 
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/figdetector", methods = ["POST"])
 def figdetector():
-   request_data = request.get_json()
-   return request_data
-    
+   file = request.files["file"]
+   img = Image.open(file.stream)
+   return jsonify({"Size": img.size})
 
 if __name__ == "__main__":
     app.run()
--- request_tests.py
+++ request_tests.py
@@ -1,6 +1,11 @@
 import requests
 
 api = "http://127.0.0.1:5000/figdetector"
+img_path = "/Users/pierrequereuil/Desktop/head.jpg"
 
-response = requests.post(api, json={"text":"Testing Message"})
+files = {
+    "file": open(img_path, "rb")
+}
+
+response = requests.post(api, files=files)
 print(response.json())
