d18742b40ab39823032416993ba019c341acc4b0
dubitable <pierrequereuil@gmail.com>
2021-07-25 13:36:17 -0700

    Reorganized file structure

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
 from flask import Flask, request, jsonify
 from predictions import load_model, predict
 from PIL import Image
+import json, os
 
 models = {
     "fig": load_model("fig")
 }
+
 UPLOAD_FOLDER = "/uploads"
 
 app = Flask(__name__, static_url_path = "/static")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/figdetector", methods = ["POST"])
+def get_info(model_name):
+    filename = os.path.join("static", model_name, "info.json")
+    with open(filename, "r") as file:
+        return json.loads(file.read())
+
+def format_predictions(predictions, model_name):
+    predictions = predictions.numpy().tolist()
+    info = get_info(model_name)
+    return {class_name : str(prediction) for class_name, prediction in zip(info["class_names"], predictions)}
+
+@app.route("/figdetector", methods = ["GET", "POST"])
 def figdetector():
-   file = request.files["file"]
-   image = Image.open(file.stream)
-   prediction = predict(image, models["fig"])
-   return jsonify({"prediction": prediction.numpy().tolist()})
+    if request.method == "GET":
+        return jsonify(get_info("fig"))
+    file = request.files["file"]
+    image = Image.open(file.stream)
+    predictions = predict(image, models["fig"])
+    formatted_predictions = format_predictions(predictions, "fig")
+    return jsonify(formatted_predictions)
 
 if __name__ == "__main__":
     app.run()
