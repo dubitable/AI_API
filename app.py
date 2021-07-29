459e35f907526dbd953a9dd1a08651b1cf42461b
dubitable <pierrequereuil@gmail.com>
2021-07-29 10:55:05 -0700

    Added CORS support and testing

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from predictions import load_model, predict
 from PIL import Image
-import json, os
+import json, os, sys
 
 models = {
     "fig": load_model("fig")
 }
+
+with open("headers.json", "r") as file:
+    headers = json.loads(file.read())
+
+print(headers)
 
 UPLOAD_FOLDER = "/uploads"
 
@@ -22,15 +27,22 @@ def format_predictions(predictions, model_name):
     info = get_info(model_name)
     return {class_name : str(prediction) for class_name, prediction in zip(info["class_names"], predictions)}
 
-@app.route("/figdetector", methods = ["GET", "POST"])
-def figdetector():
-    if request.method == "GET":
-        return jsonify(get_info("fig"))
-    file = request.files["file"]
-    image = Image.open(file.stream)
-    predictions = predict(image, models["fig"])
-    formatted_predictions = format_predictions(predictions, "fig")
-    return jsonify(formatted_predictions)
+def make_response(dictionary):
+    response = Response(json.dumps(dictionary))
+    for key, value in headers.items():
+        response.headers[key] = value
+    return response
+
+@app.route("/figdetector", methods = ["GET", "POST", "OPTIONS"])
+def figdetector():     
+    if request.method == "POST":
+        file = request.files["file"]
+        image = Image.open(file.stream)
+        print(image.size, file=sys.stdout)
+        predictions = predict(image, models["fig"])
+        formatted_predictions = format_predictions(predictions, "fig")
+        return make_response(formatted_predictions)
+    return make_response(get_info("fig"))
 
 if __name__ == "__main__":
     app.run()
---
+++ testing/request.py
@@ -0,0 +1,13 @@
+import requests
+
+local = "http://127.0.0.1:5000/figdetector"
+api = "https://detectors.herokuapp.com/figdetector"
+img_path = "/Users/pierrequereuil/Desktop/head.jpg"
+
+files = {
+    "file": open(img_path, "rb")
+}
+
+
+response = requests.post(local, files=files)
+print(response.content)
